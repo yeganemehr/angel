142b80ca8ec02912796409c742a28afbf2d1f8cc
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2020-02-13 01:33:16 +0330

    dynamic products

---
+++ header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>گالری آنجل</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="assets/packages/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/packages/bootstrap-rtl/css/bootstrap-rtl.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/packages/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/fonts.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<script src="assets/packages/jquery/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/packages/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="header">
+	<div class="header-navbar container">
+		<ul class="menu">
+			<li><a href="/products">محصولات</a></li>
+			<li><a href="/order">سفارشات خاص</a></li>
+			<li><a href="/blog">بلاگ</a></li>
+			<li><a href="/testimonial">نظرات خریداران</a></li>
+			<li><a href="/contact">تماس باما</a></li>
+		</ul>
+	</div>
+	<div class="slide">
+		<img src="assets/images/logo2.png">
+		<div class="text">گالری آنجل</div>
+	</div>
+</div>
--- index.php
+++ index.php
@@ -1,101 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>گالری آنجل</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="assets/packages/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/packages/bootstrap-rtl/css/bootstrap-rtl.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/packages/font-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/fonts.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
-	<script src="assets/packages/jquery/js/jquery-3.4.1.min.js"></script>
-	<script src="assets/packages/bootstrap/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="header">
-	<div class="header-navbar container">
-		<ul class="menu">
-			<li><a href="/products">محصولات</a></li>
-			<li><a href="/order">سفارشات خاص</a></li>
-			<li><a href="/blog">بلاگ</a></li>
-			<li><a href="/testimonial">نظرات خریداران</a></li>
-			<li><a href="/contact">تماس باما</a></li>
-		</ul>
-	</div>
-	<div class="slide">
-		<img src="assets/images/logo2.png">
-		<div class="text">گالری آنجل</div>
-	</div>
-</div>
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", true);
+include "header.php";
+include "library/MysqliDb.php";
+$db = new MysqliDb ('localhost', 'root', 'yeganemehr', 'angel');
+?>
 <section class="container" id="products">
 	<header>
 		<h2>محصولات</h2>
 		<p>محبوب ترین و خلاقانه ترین محصولات ما را اینجا مشاهده کنید، اما بیاد داشته باشید که این تنها گوشه ای از طیف گسترده محصولات آنجل است که با استفاده از منوی بالا قادر به مشاهده همه آن ها هستید.</p>
 	</header>
+	<?php
+	for ($y = 0; $y < 2; $y++) {
+	?>
 	<div class="row">
+	<?php
+		$products = $db->get("products", array($y * 3, 3));
+		for ($x = 0; $x < 3; $x++) {
+		?>
 		<div class="col-lg-4">
 			<div class="product">
-				<img src="assets/images/products/product-1.jpg">
+				<img src="<?php echo $products[$x]['image']; ?>">
 				<div class="cover">
-					<p class="cover-header">ساده کاربردی زیبا</p>
-					<p>میتونید روی کار و تزیین کنید یا همینطور ساده وشیک استفاده کنید.</p>
-					<a class="btn btn-custom" href="/product-1"><i class="fa fa-shopping-cart"></i> خرید</a>
+					<p class="cover-header"><?php echo $products[$x]['title']; ?></p>
+					<p><?php echo $products[$x]['description']; ?></p>
+					<a class="btn btn-custom" href="/product-<?php echo $products[$x]['id']; ?>"><i class="fa fa-shopping-cart"></i> خرید</a>
 				</div>
 			</div>
 		</div>
-		<div class="col-lg-4">
-			<div class="product">
-				<img src="assets/images/products/product-2.jpg">
-				<div class="cover">
-					<p class="cover-header">رومیزی</p>
-					<p>اینم رومیزی ک میتونه برای اپن نهارخوری میزمبل..دراور و......تهیه بشه.تک رنگ این کارهم خاص وزیبا میشه.</p>
-					<a class="btn btn-custom" href="/product-2"><i class="fa fa-shopping-cart"></i> خرید</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-lg-4">
-			<div class="product">
-				<img src="assets/images/products/product-3.jpg">
-				<div class="cover">
-					<p class="cover-header">پترن یک رومیزی جذاب</p>
-					<p>طرح ساده یی هست و میتونید ازکتان ..مرسریزه و دیوا استفاده کنید ک رنگهای متنوع دارن.</p>
-					<a class="btn btn-custom" href="/product-3"><i class="fa fa-shopping-cart"></i> خرید</a>
-				</div>
-			</div>
-		</div>
+		<?php } ?>
 	</div>
-	<div class="row">
-		<div class="col-lg-4">
-			<div class="product">
-				<img src="assets/images/products/product-4.jpg">
-				<div class="cover">
-					<p class="cover-header">رومیزی بزرگ ساده بافت وشیک</p>
-					<p>باپترنی راحت ک فقط چندنوع پایه وزنجیره ی ساده دارد.</p>
-					<a class="btn btn-custom" href="/product-4"><i class="fa fa-shopping-cart"></i> خرید</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-lg-4">
-			<div class="product">
-				<img src="assets/images/products/product-5.jpg">
-				<div class="cover">
-					<p class="cover-header">شش ضلعی</p>
-					<p>خاص وپرانرژی پرازرنگ .پترن کارهست.با شش ضلعی ها میشه طرحهای خاصی ارائه داد.</p>
-					<a class="btn btn-custom" href="/product-5"><i class="fa fa-shopping-cart"></i> خرید</a>
-				</div>
-			</div>
-		</div>
-		<div class="col-lg-4">
-			<div class="product">
-				<img src="assets/images/products/product-6.jpg">
-				<div class="cover">
-					<p class="cover-header">اهل بلاک کردن خسته هانیستم</p>
-					<p>وقتشم ندارم و چیزی ک خودم دوست دارم و از بافتش لذت میبرم به اشتراک میزارم</p>
-					<a class="btn btn-custom" href="/product-6"><i class="fa fa-shopping-cart"></i> خرید</a>
-				</div>
-			</div>
-		</div>
-	</div>
+	<?php } ?>
 	<p class="text-center">
 		<a class="btn btn-more" href="/products">بقیه محصولات</a>
 	</p>
